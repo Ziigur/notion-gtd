aa52d28138d583580ed7f82e84776497b04ac04d
Ziigur <zigur94@gmail.com>
2023-05-27 16:17:52 +0300

    refactor: Extract NotionCredentialsView component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,7 @@
-import { useState } from "react";
-import { Input } from "./Input";
-import { Button } from "./Button";
+import { NotionCredentialsView } from "./views/NotionCredentialsView";
 
 const App = () => {
-  const [apiKey, setApiKey] = useState("");
-  const [dbId, setDbId] = useState("");
-
-  return (
-    <div className="p-8 pt-32">
-      <h1 className="text-4xl font-medium">Notion GTD</h1>
-      <p className="text-gray-500 text-xl">Welcome to use Notion GTD!</p>
-      <div className="grid grid-cols-1 gap-4 mt-8">
-        <p className="text-gray-500 text-sm">
-          This app uses Notion to store all the GTD data. Please provide your
-          API details to get started.
-        </p>
-        <Input
-          placeholder="API Key"
-          value={apiKey}
-          onChange={(event) => {
-            setApiKey(event.currentTarget.value);
-          }}
-        />
-        <Input
-          placeholder="Database ID"
-          value={dbId}
-          onChange={(event) => {
-            setDbId(event.currentTarget.value);
-          }}
-        />
-        <Button
-          onClick={() => {
-            console.log("todo");
-          }}
-        >
-          Continue
-        </Button>
-      </div>
-    </div>
-  );
-}
+  return <NotionCredentialsView />;
+};
 
 export default App;
---
+++ src/views/NotionCredentialsView.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Button } from "../Button";
+import { Input } from "../Input";
+
+export const NotionCredentialsView = () => {
+  const [apiKey, setApiKey] = useState("");
+  const [dbId, setDbId] = useState("");
+
+  return (
+    <div className="p-8 pt-32">
+      <h1 className="text-4xl font-medium">Notion GTD</h1>
+      <p className="text-gray-500 text-xl">Welcome to use Notion GTD!</p>
+      <div className="grid grid-cols-1 gap-4 mt-8">
+        <p className="text-gray-500 text-sm">
+          This app uses Notion to store all the GTD data. Please provide your
+          API details to get started.
+        </p>
+        <Input
+          placeholder="API Key"
+          value={apiKey}
+          onChange={(event) => {
+            setApiKey(event.currentTarget.value);
+          }}
+        />
+        <Input
+          placeholder="Database ID"
+          value={dbId}
+          onChange={(event) => {
+            setDbId(event.currentTarget.value);
+          }}
+        />
+        <Button
+          onClick={() => {
+            console.log("todo");
+          }}
+        >
+          Continue
+        </Button>
+      </div>
+    </div>
+  );
+};
